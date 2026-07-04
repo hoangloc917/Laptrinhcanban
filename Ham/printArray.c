d6ba896d2699f7bc6c0c7cd6b6da212ea0b773f3
hoangloc917 <hoangloc917@gmail.com>
2026-07-04 19:57:07 +0700

    IN ra A[i] theo cot tu phai qua trai bang de quy

---
+++ Ham/printArray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void printArray(int a[],int n){
+    if (n!=0){
+        printf("%d\n",a[n-1]);
+        printArray(a, n-1);
+    }else{
+
+    }
+}
+int main ()
+{
+    int n;
+    int a[1000];
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    } 
+    printArray(a, n);
+    return 0;
+}
