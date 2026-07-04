f4f8e653a377917e3fd377099c3737e3ca280b61
hoangloc917 <hoangloc917@gmail.com>
2026-07-04 11:08:34 +0700

    Hoan Thanh

--- Ham/printPrimeNumbers.c
+++ Ham/printPrimeNumbers.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
-void printPrimeNumbers(int a,int b){
+#include <math.h>
+int isprime(int n){
+	if (n<=1) return 0;
+			for (int j=2;j<=sqrt(n);j++){
+			    if (n%j==0){
+					return 0;
+				} 
+		} return 1;
+}
+void printPrimeNumbers(int a[],int n){
 	int count=0;
-	for (int i=a; i<=b;i++){
-		for (int j=2;j<i;j++){
-			if (i%j==0){
-				count=1;
-				break;
-			}
+	for  (int i=0;i<n;i++){
+		if (isprime(a[i])){
+			count++;
 		}
-		if (count==0){
-			printf ("%d ",i);
-		}
-		count=0;
 	}
-	
+	printf("\n%d\n",count);
+	for  (int i=0;i<n;i++){
+		if (isprime(a[i])){
+			printf("%d ",a[i]);
+		}
+	}
 }
 int main ()
 {
-	int a,b;
-	scanf("%d%d",&a,&b);
-	printPrimeNumbers(a, b);
+	int n;
+	scanf("%d",&n);
+	int a[1000];
+	for (int i=0; i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	for (int i=0; i<n;i++){
+		printf("%d ",a[i]);
+	}
+	isprime(n);
+	printPrimeNumbers(a, n);
 	return 0;
 }
