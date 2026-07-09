620533b10f38f1abad6ca7f27cfcbd678fd7ab65
hoangloc917 <hoangloc917@gmail.com>
2026-07-09 12:24:31 +0700

    cho phép chèn ký tự c vào vị trí p trong chuỗi st

---
+++ Chuoi/insert.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+void Insert(char str[], char c, int p)
+{
+    int len = strlen(str);
+    if (str[len - 1] == '\n')
+    {
+        str[len - 1] = '\0';
+    }
+    for (int i = len; i >= p; i--)
+    {
+        str[i + 1] = str[i];
+    }
+    str[p] = c;
+}
+int main()
+{
+    char str[500];
+    fgets(str, 500, stdin);
+    printf("%s", str);
+    Insert(str, '-', 4);
+    printf("%s", str);
+    return 0;
+}
