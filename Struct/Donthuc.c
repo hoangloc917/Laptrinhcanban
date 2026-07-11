e7a33106efdc9cbbb3d65ac92d056e7d96f85ed3
hoangloc917 <hoangloc917@gmail.com>
2026-07-11 19:14:29 +0700

    Point a

--- Struct/Donthuc.c
+++ Struct/Donthuc.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-struct DonThuc
+typedef struct
 {
     float he_so;
     int bac;
-};
-void InDonThuc(struct DonThuc d)
+} DonThuc;
+void InDonThuc(DonThuc d)
 {
     printf("%.2fx^%d\n", d.he_so, d.bac);
 }
 int main()
 {
-    struct DonThuc d = {1.5, 5};
+    DonThuc d = {1.5, 5};
     InDonThuc(d);
     return 0;
 }
