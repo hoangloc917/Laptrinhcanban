2c9a41f78995dc5ab511cac6928334ad6dc536b1
hoangloc917 <hoangloc917@gmail.com>
2026-07-14 09:48:54 +0700

    sử dụng cấp phát động

---
+++ Pointer/pointerA.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+    int *a;
+    int n;
+    scanf("%d",&n);
+    a = (int*)malloc(n*sizeof(int));
+    for (int i=0;i<n;i++){
+        scanf("%d",a+i);
+    }
+    for (int i=0;i<n;i++){
+        printf("%d ",*(a+i));
+    }
+    free(a);
+    return 0;
+}
