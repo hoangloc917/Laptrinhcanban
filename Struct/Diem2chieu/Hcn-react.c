8c0cd930e3af0e22d987bf2d429497e416ebeeda
hoangloc917 <hoangloc917@gmail.com>
2026-07-16 18:23:46 +0700

    Xét điểm bất kì có nằm trong hình chữ nhật hay không

--- Struct/Diem2chieu/Hcn-react.c
+++ Struct/Diem2chieu/Hcn-react.c
@@ -10,9 +10,30 @@ struct Rect
     struct Point low_left;
     struct Point Up_right;
 };
+int isIn(struct Point M, struct Rect r)
+{
+    double min_X = (r.low_left.X < r.Up_right.X) ? r.low_left.X : r.Up_right.X;
+    double max_X = (r.low_left.X < r.Up_right.X) ? r.Up_right.X : r.low_left.X;
+    double min_Y = (r.low_left.Y < r.Up_right.Y) ? r.low_left.Y : r.Up_right.Y;
+    double max_Y = (r.low_left.Y < r.Up_right.Y) ? r.Up_right.Y : r.low_left.Y;
+    if ((min_X <= M.X && M.X <= max_X) && (min_Y <= M.Y && M.Y <= max_Y))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 int main()
 {
-    struct Rect a = {{5, 7}, {1, 10}};
-    printf("(%.1f, %.1f)\n", a.low_left.X, a.Up_right.Y);
+    struct Rect r = {{10, 2}, {2, 8}};
+
+    struct Point M = {10.01, 6};
+
+    if (isIn(M, r))
+        printf("YES");
+    else
+        printf("NO");
     return 0;
 }
